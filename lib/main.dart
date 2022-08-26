4267370f6c9de1a336a3d94541da3bc5a2da9750
wukgdu <wukgdu365@yahoo.com>
2022-08-26 16:38:05 +0800

    fix previous commit again

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 import './globalvars.dart';
 import './pages/home.dart';
--- lib/views/utils.dart
+++ lib/views/utils.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
-import 'package:fwfh_selectable_text/fwfh_selectable_text.dart';
+// import 'package:fwfh_selectable_text/fwfh_selectable_text.dart';
+
+// https://github.com/daohoangson/flutter_widget_from_html/tree/master/packages/fwfh_selectable_text
+mixin SelectableTextFactory on WidgetFactory {
+  /// Controls whether text is rendered with [SelectableText] or [RichText].
+  ///
+  /// Default: `true`.
+  bool get selectableText => true;
+
+  /// The callback when user changes the selection of text.
+  ///
+  /// See [SelectableText.onSelectionChanged].
+  SelectionChangedCallback? get selectableTextOnChanged => null;
+
+  @override
+  Widget? buildText(BuildMetadata meta, TextStyleHtml tsh, InlineSpan text) {
+    if (selectableText &&
+        meta.overflow == TextOverflow.clip &&
+        text is TextSpan) {
+      return SelectableText.rich(
+        text,
+        maxLines: meta.maxLines > 0 ? meta.maxLines : null,
+        textAlign: tsh.textAlign ?? TextAlign.start,
+        textDirection: tsh.textDirection,
+        textScaleFactor: 1.0,
+        onSelectionChanged: selectableTextOnChanged,
+        cursorWidth: 0,
+      );
+    }
+
+    return super.buildText(meta, tsh, text);
+  }
+}
 
 class MyWidgetFactory extends WidgetFactory with SelectableTextFactory {
 
