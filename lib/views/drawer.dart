43354e9809fad8f9ba391367dd7435afb237274d
wukgdu <wukgdu365@yahoo.com>
2022-09-24 21:03:01 +0800

    small change in drawer

--- lib/views/drawer.dart
+++ lib/views/drawer.dart
@@ -35,16 +35,11 @@ class MyDrawer extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10, bottom: 10),
-            constraints: const BoxConstraints(
-              // maxHeight: 300,
-              // maxWidth: dWidth,
-            ),
+            padding: const EdgeInsets.only(top: 0, left: 0, right: 0, bottom: 0),
             child: Image.asset(Theme.of(context).brightness == Brightness.dark ? 'assets/image/wei_grey.jpg' : 'assets/image/wei.jpg'),
           ),
           Expanded(
             child: ListView(
-              padding: const EdgeInsets.only(top: 0, left: 10, right: 10),
               children: <Widget>[
                 _oneItem(context, "/home", icon: const Icon(Icons.home), text: const Text('首页'), idx: 0),
                 _oneItem(context, "/zone", icon: const Icon(Icons.list), text: const Text('版面目录'), idx: 1),
