78d2271791fa52e0e38214282f167b9938cbe5e8
wukgdu <wukgdu365@yahoo.com>
2023-05-06 00:16:41 +0800

    more ui changes

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -12,40 +12,36 @@ class AboutPage extends StatelessWidget {
   final _contentStyle = const TextStyle(fontSize: 16);
 
   Widget addAuthor(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("开发", style: _titleStyle,),
-            GestureDetector(
-              onTap: () {
-                nv2Push(context, '/user', arguments: "22776");
-              },
-              child: Text("onepiece@bdwm", style: _contentStyle.merge(textLinkStyle)),
-            )
-          ],
-        ),
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("开发", style: _titleStyle,),
+          GestureDetector(
+            onTap: () {
+              nv2Push(context, '/user', arguments: "22776");
+            },
+            child: Text("onepiece@bdwm", style: _contentStyle.merge(textLinkStyle)),
+          )
+        ],
       ),
     );
   }
   Widget oneItem(String header, String content, {bool? isLink=false, String? addLink}) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(header, style: _titleStyle,),
-            if (isLink!=true)
-              SelectableText(content, style: _contentStyle,)
-            else if (isLink==true)
-              HtmlComponent('<a href=$content>$content</a>', ts: _contentStyle,)
-            else if (addLink!=null)
-              HtmlComponent('<a href=$addLink>$content</a>', ts: _contentStyle,),
-          ],
-        ),
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(header, style: _titleStyle,),
+          if (isLink!=true)
+            SelectableText(content, style: _contentStyle,)
+          else if (isLink==true)
+            HtmlComponent('<a href=$content>$content</a>', ts: _contentStyle,)
+          else if (addLink!=null)
+            HtmlComponent('<a href=$addLink>$content</a>', ts: _contentStyle,),
+        ],
       ),
     );
   }
@@ -61,12 +57,18 @@ class AboutPage extends StatelessWidget {
       body: ListView(
         children: [
           oneItem("北大未名BBS", "https://bbs.pku.edu.cn", isLink: true),
+          const Divider(),
           oneItem("关于此应用", "北大未名BBS第三方安卓客户端"),
+          const Divider(),
           // oneItem("开发", "onepiece@bdwm"),
           addAuthor(context),
+          const Divider(),
           oneItem("当前版本", curVersionForBBS),
+          const Divider(),
           oneItem("站内更新", innerLinkForBBS, isLink: true),
+          const Divider(),
           oneItem("开源", "https://github.com/wukgdu/bdwm_viewer", isLink: true),
+          const Divider(),
           oneItem("下载", "https://github.com/wukgdu/bdwm_viewer/releases", isLink: true),
         ],
       ),
